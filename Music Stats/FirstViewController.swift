62adbbec174a75bd9eee6aae54f3f12a7dafc765
Zac Garby <me@zacgarby.co.uk>
2018-03-28 17:59:45 +0100

    Initial Commit

---
+++ Music Stats/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Music Stats
+//
+//  Created by Zac Garby on 28/03/2018.
+//  Copyright © 2018 Zac Garby. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
